84f728766c6754979e1ae845da6d775df484f093
Paul <paulgc@cs.wisc.edu>
2016-09-19 17:22:45 -0500

    updtae code

--- py_stringsimjoin/apply_rf/executor.pyx
+++ py_stringsimjoin/apply_rf/executor.pyx
@@ -1,6 +1,6 @@
 
-def execute_plan(plan, ltable, rtable, l_key_attr, r_key_attr,                            
-                 l_match_attr, r_match_attr, feature_table, n_jobs=1):
+
+def execute_plan(plan, const vector[string]& lstrings, const vector[string]& rstrings):
 
     queue = []
     queue.extend(plan.root.children)
